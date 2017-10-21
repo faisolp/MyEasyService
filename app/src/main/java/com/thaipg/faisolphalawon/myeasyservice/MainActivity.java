2019becde4d68f25c1ac75ab192fd712f39e687a
faisolp <faisolp@gmail.com>
2017-10-21 15:44:42 +0700

    Creat Main Fragment  MArk success

--- app/src/main/java/com/thaipg/faisolphalawon/myeasyservice/MainActivity.java
+++ app/src/main/java/com/thaipg/faisolphalawon/myeasyservice/MainActivity.java
@@ -3,11 +3,24 @@ package com.thaipg.faisolphalawon.myeasyservice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.thaipg.faisolphalawon.myeasyservice.fragment.MainFragment;
+
+//Main Class
 public class MainActivity extends AppCompatActivity {
 
+    // Main Method
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //เก็บ สถานะล่าสุด หากเป็น Null คือเริ่มจาก o
+
         setContentView(R.layout.activity_main);
+
+        if(savedInstanceState==null){
+            //สวมหน้ากาก
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentFragmentMain,new MainFragment()).commit();
+        }
     }
+
+
 }
