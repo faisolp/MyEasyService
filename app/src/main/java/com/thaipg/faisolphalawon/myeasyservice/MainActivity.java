95f25fe90538a600511b4303030e764777a4b686
faisolp <faisolp@gmail.com>
2017-10-22 13:45:25 +0700

    Drawer Menu return Click

--- app/src/main/java/com/thaipg/faisolphalawon/myeasyservice/MainActivity.java
+++ app/src/main/java/com/thaipg/faisolphalawon/myeasyservice/MainActivity.java
@@ -1,12 +1,18 @@
 package com.thaipg.faisolphalawon.myeasyservice;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.thaipg.faisolphalawon.myeasyservice.fragment.MainFragment;
 
 //Main Class
 public class MainActivity extends AppCompatActivity {
+    //Set variable
+    private DrawerLayout drawerLayout;
 
     // Main Method
     @Override
@@ -15,12 +21,70 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        //Add frg to Activity
+        addFrgToActivity(savedInstanceState);
+
+
+        // Setup Drawer Menu
+        SetupDrawerMenu();
+
+        //Text Control
+        textMenuControl();
+
+        //End Drawer Menu
+
+    }//END Main Method
+
+    private void textMenuControl() {
+        TextView mainTextView = (TextView) findViewById(R.id.txtFrgMainManu);
+        TextView secondTextView = (TextView)findViewById(R.id.txtFrgSecondMenu);
+        TextView exitTextView =  (TextView) findViewById(R.id.txtExitMenu);
+
+        mainTextView.setOnClickListener(new View.OnClickListener() { //Ctn +Spec
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+            }
+        });
+        secondTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+
+            }
+        });
+
+        exitTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Close Drawer
+                drawerLayout.closeDrawers();
+            }
+        });
+
+
+    }
+
+    protected void SetupDrawerMenu() {
+
+         drawerLayout = (DrawerLayout) findViewById(R.id.myDrawerLayout);
+
+    }
+
+    private void addFrgToActivity(Bundle savedInstanceState) {
         if(savedInstanceState==null){
             //สวมหน้ากาก
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.contentFragmentMain,new MainFragment()).commit();
         }
     }
+
+
+
+
+
 
 
 }
