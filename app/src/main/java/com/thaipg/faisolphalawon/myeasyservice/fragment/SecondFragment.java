2296d63ecf4ca30d4b4fda516dcadb473b6bbe4f
faisolp <faisolp@gmail.com>
2017-10-22 15:39:13 +0700

    Update Image

--- app/src/main/java/com/thaipg/faisolphalawon/myeasyservice/fragment/SecondFragment.java
+++ app/src/main/java/com/thaipg/faisolphalawon/myeasyservice/fragment/SecondFragment.java
@@ -1,5 +1,9 @@
 package com.thaipg.faisolphalawon.myeasyservice.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,14 +11,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.thaipg.faisolphalawon.myeasyservice.R;
+
+import java.time.Instant;
 
 /**
  * Created by faisolphalawon on 10/22/2017 AD.
  */
 //Mani class
 public class SecondFragment extends Fragment {
+
+    private ImageView imageViewHm;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -23,6 +33,57 @@ public class SecondFragment extends Fragment {
 
         // button control
         btnBackControl();
+
+        //Image Control
+        imageControl();
+
+    }
+
+    // Recall to Result Service
+    @Override
+    public void onActivityResult(int requestCode
+            , int resultCode
+            , Intent data) {
+
+        //requestCode =1  -->1 array select 1
+        //resultCode ผลการทำงาน
+        //ผลข้อมูล
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == getActivity().RESULT_OK){
+            try { //กัน Error
+                Uri uri = data.getData();
+                Bitmap bitmap = BitmapFactory.decodeStream(
+                        getActivity()
+                                .getContentResolver()
+                                .openInputStream(uri)
+
+                );
+                imageViewHm.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        } else  {
+            Toast.makeText(getActivity(),"Not Choose Image",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Image Control
+    private void imageControl() {
+        imageViewHm = getView().findViewById(R.id.imgHm);
+        imageViewHm.setOnClickListener(new View.OnClickListener() { //Cnt +Enter
+            @Override
+            public void onClick(View v) {
+                Intent intant =new Intent(Intent.ACTION_GET_CONTENT);
+                intant.setType("image/*");
+                startActivityForResult(Intent.createChooser(intant,"Please Choose App"), 1);
+
+
+            }
+        });
+
     }
 
     private void btnBackControl() {
